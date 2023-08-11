cb60f1c2eb85631d7f7a2e28f37852b879e34d9b
TatianeMarinho <devtatianemarinho@gmail.com>
2023-08-10 21:57:27 -0400

    criei os componentes do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,51 @@
 function Header() {
   return (
-    <header>meu header</header>
+    <form action="">
+      <section>
+        <label htmlFor="">
+          Projeto Star Wars - Trybe
+          <input type="text" data-testid="name-filter" />
+        </label>
+      </section>
+      <section>
+        <label htmlFor="">
+          Coluna
+          <select name="" id="">
+            <option value="">uma opçao aqui</option>
+          </select>
+        </label>
+        <label htmlFor="">
+          Operador
+          <select name="" id="">
+            <option value="">uma opçao aqui</option>
+          </select>
+        </label>
+        <label htmlFor="">
+          <input type="text" />
+        </label>
+        <button>Filtrar</button>
+        <label htmlFor="">
+          Ordenar
+          <select name="" id="">
+            <option value="">uma opçao aqui</option>
+          </select>
+        </label>
+        <label>
+          <input
+            type="checkbox"
+          />
+          Ascendente
+        </label>
+        <label>
+          <input
+            type="checkbox"
+          />
+          Descendente
+        </label>
+        <button>Ordenar</button>
+        <button>Remover Filtros</button>
+      </section>
+    </form>
   );
 }
 export default Header;
