3821ef7730d1c8cdac41a1576aeba4b771529814
TatianeMarinho <devtatianemarinho@gmail.com>
2023-08-10 21:26:51 -0400

    adicionei um header na pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import Table from './components/table';
+import Table from './components/Table';
+import Header from './components/Header';
 
 function App() {
   return (
     <main>
+      <Header />
       <Table />
     </main>
   );
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,6 @@
+function Header() {
+  return (
+    <header>meu header</header>
+  );
+}
+export default Header;
